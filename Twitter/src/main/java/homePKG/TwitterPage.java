4b1458b252fb1d9496adcf31662ccb53c1870c4a
masharif2012 <isharifpalash@gmail.com>
2019-01-06 14:48:34 -0500

    hello 1234

--- Twitter/src/main/java/homePKG/TwitterPage.java
+++ Twitter/src/main/java/homePKG/TwitterPage.java
@@ -3,4 +3,10 @@ package homePKG;
 import Base.CommonAPI;
 
 public class TwitterPage extends CommonAPI {
+
+    @Override
+    public void typeOnCssButton(String locator) {
+        super.typeOnCssButton(locator);
+        // made change
+    }
 }
