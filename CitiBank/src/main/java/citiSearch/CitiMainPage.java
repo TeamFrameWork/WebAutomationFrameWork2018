a1023257bc224bbf379f8fbc95ab7ebd12ed7721
OsmanSE <osmansalih717@gmail.com>
2019-01-11 19:43:28 -0500

    runtest update

--- CitiBank/src/main/java/citiSearch/CitiMainPage.java
+++ CitiBank/src/main/java/citiSearch/CitiMainPage.java
@@ -1,9 +1,48 @@
 package citiSearch;
 
 import Base.CommonAPI;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import javax.management.ConstructorParameters;
+
 
 public class CitiMainPage extends CommonAPI {
-    public static void main(String[] args) {
-        System.out.println("H     E   Y");
-    }
+
+   @Test
+   public void typeOnCss(String locator, String value){
+      driver.findElement(By.cssSelector(locator)).sendKeys(value);
+   }
+   @Test
+   public void typeOnId(String locator, String value){
+      driver.findElement(By.id(locator)).sendKeys(value);
+   }
+   @Test
+   public void  typeOnElement(String locator, String value){
+      try {
+
+         driver.findElement(By.cssSelector(locator)).sendKeys(value);
+      }catch (Exception e1){
+         try {
+            driver.findElement(By.name(locator)).sendKeys(value);
+         }catch (Exception e2){
+            try {
+               driver.findElement(By.xpath(locator)).sendKeys(value);
+            }catch (Exception e3){driver.findElement(By.id(locator)).sendKeys();}
+         }
+      }
+   }
+
+   public void clearField(String locator){
+      driver.findElement(By.id(locator)).clear();
+   }
+
+   public void navigateBack(){
+      driver.navigate().back();
+   }
 }
+
+
---
+++ CitiBank/src/test/java/testPackage/RunTest.java
@@ -0,0 +1,15 @@
+package testPackage;
+
+import citiSearch.CitiMainPage;
+import org.junit.Test;
+
+public class RunTest extends CitiMainPage {
+    @Test
+    public void citiTest(){
+
+    }
+
+@Test
+    public void loginPageTest(){}
+}
+
